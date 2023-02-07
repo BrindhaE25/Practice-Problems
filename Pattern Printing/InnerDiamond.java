b26d47fe0cad481b60d87dbed74eba69bd232f07
BrindhaE25 <46149163+BrindhaE25@users.noreply.github.com>
2023-02-07 21:05:30 +0530

    Update and rename innertriangle.java to InnerDiamond.java

--- Pattern Printing/InnerDiamond.java
+++ Pattern Printing/InnerDiamond.java
@@ -1,3 +1,15 @@
+/*
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+*/
 class innertriangle {
 	public static void main(String args[]) {
 		int row=5;
