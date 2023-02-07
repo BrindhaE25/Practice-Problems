36fb45d5581ff741ef061ba1ba25feaa0f5207eb
BrindhaE25 <46149163+BrindhaE25@users.noreply.github.com>
2023-02-07 20:58:55 +0530

    Update and rename diamond.java to QuarterDiamondPrinting.java

--- Pattern Printing/QuarterDiamondPrinting.java
+++ Pattern Printing/QuarterDiamondPrinting.java
@@ -1,9 +1,14 @@
+/*
+     *
+    **
+   ***
+  ****
+ *****
+ */
 class diamond {
 	public static void main(String args[]) {
 		int row =5;
 		for(int i=1;i<=row;i++) {
-			for(int j=1;j<=i;j++)
-				System.out.printf("*");
 			for(int k=1;k<=row;k++) {
 			System.out.printf("%"+ (row-k+1)+ "s"," ");
 			for(int l=1;l<=k;k++)
